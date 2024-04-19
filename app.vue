bc6387b091cc52224abb0929eb3acc5f55d145f5
Marijn van der Krieken <marijn@flyingkiwi.nl>
2024-04-19 17:17:40 +0200

    Update app.vue and nuxt.config.ts

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Test</div>
+  <div>{{ data }}</div>
+  <div>{{ new Date().toISOString() }}</div>
 </template>
+<script setup lang="ts">
+const { data } = await useFetch('/api/date')
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,36 +3,28 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   routeRules: {
     '*': {
-      swr: 60,
+      swr: 10,
     },
   },
   modules: ['nuxt-security'],
   security: {
-    enabled: false,
+    enabled: true,
     nonce: true, // Enables HTML nonce support in SSR mode
     ssg: {
       hashScripts: true,
       hashStyles: false,
     },
-    // headers: {
-    //crossOriginEmbedderPolicy: 'require-corp',
-    // contentSecurityPolicy: {
-    //   'script-src': [
-    //     "'self'",
-    //     'https:',
-    //     "'unsafe-inline'",
-    //     "'strict-dynamic'",
-    //     "'nonce-{{nonce}}'",
-    //   ],
-    //   'style-src': ["'self'", 'https:', "'unsafe-inline'"],
-    //   'img-src': ["'self'", 'data:', 'blob:'],
-    //   'font-src': ["'self'", 'https:', 'data:'],
-    //   'base-uri': ["'none'"],
-    //   'object-src': ["'none'"],
-    //   'script-src-attr': ["'none'"],
-    //   'form-action': ["'self'"],
-    //   'frame-ancestors': ["'self'"],
-    // },
-    // },
+    headers: {
+    crossOriginEmbedderPolicy: 'unsafe-none',
+    contentSecurityPolicy: {
+      'script-src': [
+        "'self'",
+        'https:',
+        "'unsafe-inline'",
+        "'strict-dynamic'",
+        "'nonce-{{nonce}}'",
+      ],
+    },
+    },
   },
 });
