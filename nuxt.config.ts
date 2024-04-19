bf37319b01f80e7ace10a8fe27106fb57e57e4cb
MarijnFK <47977238+MarijnFK@users.noreply.github.com>
2024-04-19 16:50:15 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  routeRules: {
+    '*': {
+      swr: 60,
+    },
+  },
+  modules: ['nuxt-security'],
+  security: {
+    enabled: true,
+    nonce: true, // Enables HTML nonce support in SSR mode
+    ssg: {
+      hashScripts: true,
+      hashStyles: false,
+    },
+    // headers: {
+    //crossOriginEmbedderPolicy: 'require-corp',
+    // contentSecurityPolicy: {
+    //   'script-src': [
+    //     "'self'",
+    //     'https:',
+    //     "'unsafe-inline'",
+    //     "'strict-dynamic'",
+    //     "'nonce-{{nonce}}'",
+    //   ],
+    //   'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+    //   'img-src': ["'self'", 'data:', 'blob:'],
+    //   'font-src': ["'self'", 'https:', 'data:'],
+    //   'base-uri': ["'none'"],
+    //   'object-src': ["'none'"],
+    //   'script-src-attr': ["'none'"],
+    //   'form-action': ["'self'"],
+    //   'frame-ancestors': ["'self'"],
+    // },
+    // },
+  },
+});
